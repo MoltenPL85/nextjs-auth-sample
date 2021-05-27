b18a0216227b4c7721025f748fee67be44fcf78e
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-05-27 19:29:39 +0500

    Page protection

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useRef, useState } from 'react';
 import { signIn } from 'next-auth/client';
+import { useRouter } from 'next/router';
 import classes from './auth-form.module.css';
 
 async function createUser(email, password) {
@@ -25,6 +26,7 @@ function AuthForm() {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const router = useRouter();
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
@@ -46,6 +48,7 @@ function AuthForm() {
       });
 
       if (!result.error) {
+        router.replace('/profile');
       }
     } else {
       try {
